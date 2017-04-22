b79c25f6d182a13d9343b2c515398b394ae123ee
Sajo <piotrs.web@hotmail.com>
2017-04-22 11:30:32 +0200

    cleaning project for development

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,2 @@
-import $ from 'jquery';
-import _ from 'lodash';
-import {bleh} from "shared/test";
 // webpack advanced mechanism
 import "./app.scss";
-
-bleh();
-
-$("body").html("WHOAA");
-console.log(_);
--- src/shared/test.js
+++
@@ -1,3 +0,0 @@
-export function bleh()  {
-  console.log("WORKED");
-}
